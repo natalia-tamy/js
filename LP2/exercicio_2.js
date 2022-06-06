fe2033849751575522b44c237b1b16b62761fd3a
Natalia Tamy <naty.tamy@gmail.com>
2022-06-05 21:37:21 -0300

    exercicio dois

---
+++ LP2/exercicio_2.js
@@ -0,0 +1,6 @@
+let distanciaPercorrida = 100;
+let tempoGasto = 2;
+
+let velocidadeMedia = distanciaPercorrida / tempoGasto;
+
+console.log(`A velocidade média do verículo foi de ${velocidadeMedia}km/h`)
